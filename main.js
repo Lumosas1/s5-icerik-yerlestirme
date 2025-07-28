7653bb7855b7ef1aa86ceb62e7232f61014d7ebd
Nesrin Eskiocak <nesrineskiocak68@gmail.com>
2025-07-29 00:29:39 +0300

    logo eklendi

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ import { siteContent } from "./content.js";
 console.log("Site içeriği", siteContent);
 
 /* Kodlar buradan aşağıya */
+
+const logoImg = document.getElementById("logo-img");
+logoImg.src = "./images/logo.webp";
